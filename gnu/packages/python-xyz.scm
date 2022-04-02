6b937ac89609f97a20e6ed4c23038c1188a71545
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-04-01 20:40:53 -0400

    gnu: python-fonttools-with-test: Rename to python-fonttools-full.
    * gnu/packages/python-xyz.scm (python-fonttools-with-test): Rename to...
    (python-fonttools-full): ... this.
    [native-inputs]: Move python-brotli, python-fs, python-scipy
    and python-zopfli to...
    [propagated-inputs]: ... here.  Add python-lxml, python-lz4 and
    python-unicodedata2.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -11337,7 +11337,9 @@ from an XML-based format.")
      (license license:expat)
      (properties `((python2-variant . ,(delay python2-fonttools)))))))
 
-(define-public python-fonttools-with-tests
+;;; Rename 'python-fonttools' in next cycle, renaming the current
+;;; 'python-fonttools' to 'python-fonttools-minimal'.
+(define-public python-fonttools-full
   (package/inherit python-fonttools
     (arguments
      (substitute-keyword-arguments (package-arguments python-fonttools)
@@ -11362,11 +11364,15 @@ from an XML-based format.")
                                   " and not "))))))))))
     (native-inputs
      (modify-inputs (package-native-inputs python-fonttools)
-       (append python-pytest            ;FIXME: indentation is broken
-           python-brotli
-         python-fs
-         python-scipy
-         python-zopfli)))
+       (append python-pytest)))
+    (propagated-inputs
+     (list python-brotli
+           python-fs
+           python-lxml
+           python-lz4
+           python-scipy
+           python-unicodedata2
+           python-zopfli))
     (properties (alist-delete 'hidden? (package-properties python-fonttools)))))
 
 ;; Fonttools 4.x dropped support for Python 2, so stick with 3.x here.
