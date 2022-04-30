a6a7d69ed05092cc6578ea80e9e0cd8bff8d69de
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-04-29 22:54:56 -0400

    gnu: Remove python2-pyinotify.
    * gnu/packages/python-xyz.scm (python2-pyinotify): Delete variable.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -22427,9 +22427,6 @@ interpreter. bpython's main features are
 file system events on Linux.")
     (license license:expat)))
 
-(define-public python2-pyinotify
-  (package-with-python2 python-pyinotify))
-
 
 (define-public python-more-itertools
   (package
