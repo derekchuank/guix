663b289abdfa52a13f41495a39285eaa6e624864
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-04-29 23:00:34 -0400

    gnu: Remove python2-execnet.
    * gnu/packages/python-xyz.scm (python2-execnet): Delete variable.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -14633,9 +14633,6 @@ minimal and fast API targeting the following uses:
     (home-page "https://codespeak.net/execnet/")
     (license license:expat)))
 
-(define-public python2-execnet
-  (package-with-python2 python-execnet))
-
 (define-public python-icalendar
   (package
     (name "python-icalendar")
