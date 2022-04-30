a4929d949520c868f7788e53f85f52da79dd1dac
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-04-29 23:13:09 -0400

    gnu: Remove python2-ukpostcodeparser.
    * gnu/packages/python-xyz.scm (python2-ukpostcodeparser): Delete variable.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -17442,9 +17442,6 @@ simple, lightweight implementation.")
 parsing UK postcodes.")
     (license license:expat)))
 
-(define-public python2-ukpostcodeparser
-  (package-with-python2 python-ukpostcodeparser))
-
 (define-public python-faker
   (package
     (name "python-faker")
